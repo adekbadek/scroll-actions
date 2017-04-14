1e6b15605d1383880951874339d2c34df15138ce
Adam Borowski <adam@adamboro.com>
2017-04-14 12:40:28 +0200

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: {
+    main: './main.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js' // Template based on keys in entry above
+  },
+  babel: {
+    presets: ['es2015'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ]
+  },
+  resolve: {
+    // you can now require('file') instead of require('file.es6')
+    extensions: ['', '.js', '.json']
+  },
+}
